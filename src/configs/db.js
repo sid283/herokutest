fe50329f90d23f964bba29abe46652610c720f5a
Siddalinga <siddaling.gkoudi@gmail.com>
2022-02-25 21:10:48 +0530

    small changes

--- src/configs/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose")
-
-const connect = ()=>{
-    return mongoose.connect("mongodb+srv://sid283:sid_283@cluster0.cbay8.mongodb.net/test3?retryWrites=true&w=majority")
-}
-
-module.exports = connect
