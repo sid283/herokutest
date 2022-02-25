c002529f30f261e34432b95d40be92b32a91d6e2
Siddalinga <siddaling.gkoudi@gmail.com>
2022-02-25 21:21:03 +0530

    small changes

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+
+const connect = require("./src/configs/db");
+
+const userController = require("./src/controllers/user.controller");
+const { register, login, newToken } = require("./src/controllers/auth.controller");
+
+const passport = require("./src/configs/google-oauth");
+
+const app = express();
+
+app.use(express.json());
+
+// /register
+app.post("/register", register);
+// .login
+app.post("/login", login);
+
+app.use("/users", userController);
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/auth/google/failure",
+  }),
+  (req, res) => {
+    const { user } = req;
+    const token = newToken(user);
+
+    return res.send({ user, token });
+  }
+);
+
+app.listen(2345, async () => {
+  try {
+    await connect();
+  } catch (err) {
+    console.error(err.message);
+  }
+  console.log("listening on port 2345");
+});
